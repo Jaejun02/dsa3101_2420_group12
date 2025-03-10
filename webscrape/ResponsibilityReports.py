7549433e6fa2db5a2b97dacfbd2b71cfd44fbdbe
Jaejun Shim <jaejun.shim@outlook.com>
2025-03-10 16:35:31 +0800

    Removed unused texts and .ipynb files.

--- webscrape/ResponsibilityReports.py
+++ webscrape/ResponsibilityReports.py
@@ -5,9 +5,16 @@ import os
 import logging
 
 class ResponsibilityReports:
+    """
+    This class creates a webscraper to download ESG Reports from responsibilityreports.com.
+    The class has the following functions:
+        - webscrape_report_urls: Scrapes the website to find URLs to download ESG Reports.
+        - download_helper: Downloads the PDF from the given URL and saves it to the output path. Used as a helper
+                           function of download_reports.
+        - download_reports: Downloads the ESG Reports from the URLs obtained from webscrape_report_urls function.
+    """
     DATA_DIR = "../data"
     RESPONSIBILITY_REPORTS_BASE = "https://www.responsibilityreports.com/"
-    SUSTAINABILITY_REPORTS_BASE = "https://www.sustainabilityreports.com/"
 
     def __init__(self):
         # Setting up logger
@@ -65,7 +72,7 @@ class ResponsibilityReports:
                     report_link, report_name = report['href'], report.text.strip()
                     report_link = self.RESPONSIBILITY_REPORTS_BASE + report_link
                     download_urls.append(report_link)
-                    download_dests.append(self.DATA_DIR + f"/{automotive_industry[i]}/{company_name[j]}/{report_name}.pdf")
+                    download_dests.append(self.DATA_DIR + f"/{automotive_industry[i]}-{company_name[j]}-{report_name}.pdf")
         self.logger.debug("Found %d reports to download", len(download_urls))
         return download_urls, download_dests
 
@@ -89,19 +96,19 @@ class ResponsibilityReports:
             response.raise_for_status()
             with open(output_path, 'wb') as pdf_file:
                 pdf_file.write(response.content)
-            with open(self.DATA_DIR + "/ResponsbilityReports.txt", 'a') as file:
-                file.write(f"Industry: {output_path.split("/")[2]}, Company: {output_path.split("/")[3]}\n")
-            with open(self.DATA_DIR + "/pdf_reports_urls.txt", 'a') as file:
-                file.write(url+'\n')
+            # with open(self.DATA_DIR + "/pdf_reports_paths.txt", 'a') as file:
+            #     file.write(output_path + '\n')
+            # with open(self.DATA_DIR + "/pdf_reports_urls.txt", 'a') as file:
+            #     file.write(url+'\n')
             return True
         except requests.RequestException as e:
-            self.logger.error("Error downloading the PDF: %s", e)
+            self.logger.error("Error downloading the PDF %s: %s", output_path, e)
             return False
         except IOError as e:
-            self.logger.error("Error saving the PDF: %s", e)
+            self.logger.error("Error saving the PDF %s: %s", output_path, e)
             return False
 
-    def download_reports(self):
+    def download_reports(self, scrape_url = False):
         """
         This function downloads the ESG Reports from the URLs obtained from webscrape_report_urls function.
         Input:
@@ -110,11 +117,15 @@ class ResponsibilityReports:
             Result -> String: Summary statement of the download process.
         """
         self.logger.debug("Starting download of reports.")
-        download_urls, download_dests = self.webscrape_report_urls()
+        if scrape_url:
+            download_urls, download_dests = self.webscrape_report_urls()
+        else:
+            download_urls = [line.strip() for line in open(self.DATA_DIR + "/pdf_reports_urls.txt", 'r')]
+            download_dests = [line.strip() for line in open(self.DATA_DIR + "/pdf_reports_paths.txt", 'r')]
         results = []
         for i, url in enumerate(download_urls):
             result = self.download_helper(url, download_dests[i])
             results.append(result)
         summary = f"\rSuccessfully Downloaded {sum(results)}/{len(results)} PDFs"
         self.logger.info(summary)
-        return True
+        return True, summary
